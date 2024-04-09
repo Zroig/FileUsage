a02ffd72cc1a551b55ba30fd231c7683ba9ea072
Mateo Cerna <mateocernasanchez@gmail.com>
2024-04-09 15:27:31 +0200

    EX1 Complert

--- src/Ex1/Ex1.java
+++ src/Ex1/Ex1.java
@@ -3,7 +3,6 @@ package Ex1;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
