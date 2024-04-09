3945c915502252ba143e5f23b7d1dde350e1959e
Mateo Cerna <mateocernasanchez@gmail.com>
2024-04-09 15:16:45 +0200

    BufferedWriter Added

--- src/Ex1/Ex1.java
+++ src/Ex1/Ex1.java
@@ -1,6 +1,9 @@
 package Ex1;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
@@ -23,14 +26,21 @@ public class Ex1 {
             try {
                 Path carpeta = Files.createDirectories(origin.resolve("EX1"));
                 Path fitxer = Files.createFile(carpeta.resolve("secret.txt"));
-                Files.writeString(fitxer, entrada+"\n");
+                BufferedWriter escriptor = new BufferedWriter(new FileWriter("C:\\Users\\Zroig\\Estudis\\CFGSDAW1\\MP03\\UF3\\PT1\\EX1\\secret.txt"));
+                escriptor.write(entrada);
+                escriptor.close();
+                //Files.writeString(fitxer, entrada+"\n");
             } catch (Exception e) {
                 e.printStackTrace();
             }
             
         } else {
             try {
-                Files.writeString(origin, entrada+"\n");
+                BufferedWriter escriptor = new BufferedWriter(new FileWriter("C:\\Users\\Zroig\\Estudis\\CFGSDAW1\\MP03\\UF3\\PT1\\EX1\\secret.txt"));
+                escriptor.newLine();
+                escriptor.write(entrada);
+                escriptor.close();
+                //Files.writeString(origin, entrada+"\n");
             } catch (IOException e) {
                 e.printStackTrace();
             }    
