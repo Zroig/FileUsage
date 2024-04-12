552b13029e87ded8c9d75d6c45031f9a1cc3880b
Mateo Cerna <mateocernasanchez@gmail.com>
2024-04-12 22:28:19 +0200

    EX3 Done

--- src/Ex3/Ex3.java
+++ src/Ex3/Ex3.java
@@ -1,18 +1,36 @@
 package Ex3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Ex3 {
     public static void main(String[] args) {
         boolean exists = false;
+        String linia = "";
+        int contl = 0, contp = 0;
 
         Path origin = Path.of("resultats");
-        Path fitxer = Path.of("\\EX3\\fitxer.txt");
-
+        Path fitxer = Path.of(origin+"\\EX3\\fitxer.txt");
+        
         exists = Files.exists(fitxer);
 
         if (exists) {
+            try {
+                FileReader fitxerlectura = new FileReader("resultats\\EX3\\fitxer.txt");
+                BufferedReader lector = new BufferedReader(fitxerlectura);
+
+                while ((linia = lector.readLine()) != null) {
+                    contl++;
+                    String[] paraules = linia.split(" ");
+                    contp += paraules.length;
+                }
+
+                System.out.println("Numero de linies: "+contl+"\nNumero de paraules: "+contp);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
         } else {
             System.out.println("ERROR 001: File 'resultats/EX3/fitxer.txt' does not exist");
