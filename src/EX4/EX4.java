161b9d932cc3ab204ca06021b895016992c6f6ce
Mateo Cerna <mateocernasanchez@gmail.com>
2024-04-15 16:32:13 +0200

    Practica acabada

--- src/EX4/EX4.java
+++ src/EX4/EX4.java
@@ -1,50 +1,64 @@
 package EX4;
 
-import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Scanner;
 import java.nio.file.Files;
-import java.io.FileReader;
 
 public class EX4 {
     public static void main(String[] args) {
-
+        //Llegir Ruta
         Scanner sc = new Scanner(System.in);
         String entrada = sc.next();
+        sc.close();
 
         //Comprovar existencia de fitxers
         Path fitxer = Path.of(entrada);
         boolean exists = Files.exists(fitxer) && entrada.endsWith(".txt");
 
+        //Si no existeix o no es txt donara error
         if (!exists) {
 
             System.out.println("Error 001: Fitxer no existeix o no és de format valid");
 
         } else {
-
-            HashMap<String, HashMap<Integer, Integer>> ubicacions = new HashMap<>();
             
             try {
 
                 FileReader fitxerlectura = new FileReader(entrada);
                 BufferedReader lector = new BufferedReader(fitxerlectura);
                 String linia;
+                int max = 0;
+                int min = Integer.MAX_VALUE;
+                int res = 0;
+                int cont = 0;
 
                 while ((linia = lector.readLine()) != null) {
 
                     String[] paraules = linia.split(" ");
-
+                    boolean primeraParaula = true;
                     for (int i = 0; i < paraules.length; i++){
                         if (!isInt(paraules[i])) {
-                            ubicacions.put(paraules[i], trobarVelocitats(paraules[i], lector));
+                            if (!primeraParaula) {
+                                System.out.println("Velocitat maxima: "+max+"\nVelocitat minima: "+min+"\nVelocitat Mitjana: "+res/cont);
+                            }
+                            System.out.println(paraules[i]+":");
+                            max = Integer.MIN_VALUE;
+                            min = Integer.MAX_VALUE;
+                            res = 0;
+                            cont = 0;
+                            primeraParaula = false;
+                        } else {
+                            max = Math.max(max, Integer.parseInt(paraules[i]));
+                            min = Math.min(min, Integer.parseInt(paraules[i]));
+                            cont++;
+                            res = res+Integer.parseInt(paraules[i]);
                         }
                     }
-                    //Calcular velocitats
-                    System.out.println(ubicacions.get("Roma"));
                 }
+                System.out.println("Velocitat maxima: "+max+"\nVelocitat minima: "+min+"\nVelocitat Mitjana: "+res/cont);
+                lector.close();
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -55,52 +69,6 @@ public class EX4 {
 
         
         
-    }
-
-    public static HashMap<Integer, Integer> trobarVelocitats (String clau, BufferedReader lector) {
-
-        HashMap<Integer, Integer> velocitats = new HashMap<>();
-        String lastString = "";
-        String linia;
-        boolean foundClau = false;
-        int j = 0;
-
-        try {
-
-            while ((linia = lector.readLine()) != null) {
-                String[] paraules = linia.split(" ");
-    
-                for (int i = 0; i < paraules.length; i++){
-    
-                    if (!isInt(paraules[i]) && foundClau == false) {
-    
-                        lastString = paraules[i];
-                        foundClau = lastString == clau ? true : false;
-                    
-                    } else {
-    
-                        if (lastString == clau) {
-    
-                            velocitats.put(j, Integer.parseInt(paraules[i]));
-                            j++;
-                        }
-    
-                        if (foundClau == true && lastString != clau) {
-                            break;
-                        }
-                    }
-                }
-                
-            }
-
-        } catch (IOException e) {
-
-            e.printStackTrace();
-            return null;
-        }
-        
-
-        return velocitats;
     }
     
     public static boolean isInt(String valor){
